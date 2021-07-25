370efdc2d4841f00b3067a09395279f96b376d40
vivek <vivek747raj@gmail.com>
2021-07-25 20:23:18 +0530

    adding test file

---
+++ src/test/java/Academy/Test.java
@@ -0,0 +1,15 @@
+package Academy;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         //a
+         //b
+         //c
+         //d
+         //e
+         //f
+	}
+
+}
